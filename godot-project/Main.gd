dcab68c925764bbf4bb1894ed04dbe3ccaeb46ba
Henrique Alves <henriquelalves@gmail.com>
2022-07-21 01:29:47 -0300

    Start from the client

--- godot-project/Main.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-func _ready():
-	randomize()
-	var isClient = ProjectSettings.get_setting("Config/IsClient")
-	if (isClient):
-		get_tree().change_scene("res://Client/Client.tscn")
-	else:
-		get_tree().change_scene("res://Server/Server.tscn")
